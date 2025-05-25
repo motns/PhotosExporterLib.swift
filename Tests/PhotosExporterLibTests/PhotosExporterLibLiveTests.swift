566764ac76324f8d4d34d4fae82bb1ab63c3c777
Adam Borocz <github@motns.io>
2025-05-25 04:19:45 +0100

    Basic live integration test
    Should be run inside a MacOS VM with the Photos Library bundled with this repo put in place

--- Tests/PhotosExporterLibTests/PhotosExporterLibLiveTests.swift
+++ Tests/PhotosExporterLibTests/PhotosExporterLibLiveTests.swift
@@ -11,7 +11,59 @@ import Testing
   )
 )
 final class PhotosExporterLibLiveTests {
+  let photosExporterLib: PhotosExporterLib
+  let exporterDB: ExporterDB
+  let testDir: String
+  let timeProvider: TestTimeProvider
+
+  init() async throws {
+    var logger = Logger(label: "io.motns.testing")
+    logger.logLevel = .critical
+    self.testDir = try TestHelpers.createTestDir()
+    self.timeProvider = TestTimeProvider()
+
+    self.exporterDB = try ExporterDB(
+      exportDBPath: testDir + "/export.sqlite",
+      logger: logger,
+    )
+
+    self.photosExporterLib = try await PhotosExporterLib(
+      exportBaseDir: testDir,
+      loggerOpt: logger,
+    )
+  }
+
+  deinit {
+    if FileManager.default.fileExists(atPath: testDir) {
+      try? FileManager.default.removeItem(atPath: testDir)
+    }
+  }
+
   @Test("Export")
   func export() async throws {
+    let expectedInitialRes = ExportResult(
+      assetExport: AssetExportResult(
+        assetInserted: 8,
+        assetUpdated: 0,
+        assetUnchanged: 0,
+        assetSkipped: 0,
+        fileInserted: 8,
+        fileUpdated: 0,
+        fileUnchanged: 0,
+        fileSkipped: 0
+      ),
+      collectionExport: CollectionExportResult(
+        folderInserted: 1,
+        folderUpdated: 0,
+        folderUnchanged: 0,
+        albumInserted: 0,
+        albumUpdated: 0,
+        albumUnchanged: 0
+      ),
+      fileCopy: FileCopyResult(copied: 8, removed: 0)
+    )
+
+    let initialRes = try await photosExporterLib.export()
+    #expect(initialRes == expectedInitialRes)
   }
 }
